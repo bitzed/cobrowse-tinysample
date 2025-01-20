93babfec9318abdbb592ca790920e103d8d4dc95
bizted <offpiste8510@gmail.com>
2025-01-20 15:59:15 +0900

    tidy up comments and console logs

--- public/customer.js
+++ public/customer.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-  console.log('DOMContentLoaded イベントが発生しました。');
+  console.log('DOMContentLoaded');
   try {
     getSdkToken().then(sdkToken => {
       initializeCobrowseSDK(sdkToken);
     });
   } catch (error) {
-    console.error('トークンの取得中にエラーが発生しました:', error);
+    console.error('failed to get token:', error);
   }
 });
   
@@ -19,24 +19,22 @@ async function getSdkToken() {
       body: JSON.stringify({ role: 1 })
     });
     if (!response.ok) {
-      throw new Error(`HTTP エラー! ステータス: ${response.status}`);
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
     const data = await response.json();
-    console.log('SDK トークンを取得しました:', data.sdkToken);
+    console.log('SDK token:', data.sdkToken);
     return data.sdkToken;
   } catch (error) {
-    console.error('トークンの取得中にエラーが発生しました:', error);
+    console.error('failed to get token:', error);
     throw error;
   }
 }
 
 function initializeCobrowseSDK(sdkToken) {
-  console.log('Cobrowse SDK の初期化を開始します。');
   if (typeof ZoomCobrowseSDK === 'undefined') {
-    console.log('ZoomCobrowseSDK が読み込まれていません。');
+    console.log('SDK is not loaded yet.');
     return;
   }
-  console.log('ZoomCobrowseSDK が読み込まれました。');
   const settings = {
     allowCustomerAnnotation: true,
     piiMask:{
@@ -46,16 +44,15 @@ function initializeCobrowseSDK(sdkToken) {
   };
 
   ZoomCobrowseSDK.init(settings, function({ success, session, error }) {
-      console.log('Cobrowse SDK の初期化が完了しました。');
     if (success) {
-      console.log('Cobrowse SDK の初期化に成功しました。');
+      console.log('SDK has been initialized successfully.');
       const startButton = document.getElementById('startCobrowse');
       startButton.disabled = false;
       startButton.addEventListener('click', function() {
         startCobrowseSession(session, sdkToken);
       });
     } else {
-      console.error('Cobrowse SDK の初期化中にエラーが発生しました:', error);
+      console.error('Failed to initialize SDK:', error);
     }
   });
 }
@@ -64,10 +61,10 @@ function startCobrowseSession(session, sdkToken) {
   session.start({
     sdkToken: sdkToken,
     onSuccess: function() {
-      console.log('Cobrowse セッションの開始に成功しました。');
+      console.log('Session has been started successfully.');
     },
     onError: function(error) {
-      console.error('Cobrowse セッションの開始中にエラーが発生しました:', error);
+      console.error('Failed to start session:', error);
     }
   });
 }
